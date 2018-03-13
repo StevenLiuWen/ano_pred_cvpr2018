4ad77cabf91f25d9bac7cc94bb8c324ee0cd1630
StevenLiuWen <liuwen@shanghaitech.edu.cn>
2018-03-13 04:43:10 -0400

    Update download_pretrains.sh

--- Codes/models/download_pretrains.sh
+++ Codes/models/download_pretrains.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading trained models on ped1, ped2 and avenue datasets ....."
 
-wget "https://ofhz9a.bn.files.1drv.com/y4mHfGdUxGoa7NnnI-eIlTqInymvmHyDOSGGw5zKM08jOGukHKdYdxmtZiEEh-rCAWK7oTDTstQ5bKazvjdyTtsIUW7zxcKnVgIsgZg6DpEb-Qdq83Zmnnw6nv7pX5HhiOkMxc42CLl65QK0A2Mv1Cmj-062Pyodm-Mt5r24Id3_glS0NT6BdvAp7-VbevkXygnmXQrcXRQU6d0y1cHlZJ2ig/pretrains.tar.gz"
+wget "https://ofhz9a.bn.files.1drv.com/y4mljH6-fb1fLS6B9cJg5kst-wanKhmgoA8mWk_8y8atGlL4SQqEqZRljA8LuLkxkvm-36PArbAsx-uYKxzfUX1s8otO8h0Rv7tag9Agx1h8_RsTPhQo-aft0QSybwpvcvOkegqfLVg2UJjTzIsxgizIYCVDIpunmpasyB3t4mchRGZetZQGiKP4fS2Vfb4c3tj1jxzpN61Pmou9QaB3VAG0w/pretrains.tar.gz"
 tar -xvf pretrains.tar.gz
 rm pretrains.tar.gz
 
